4eecdac73d781ff8720f81fd97c91fe210d42aa3
cabula <cabula@bancolombia.com.co>
2023-09-30 14:01:51 -0500

    Init project

---
+++ src/test/java/com/helpcenter/assigneemanagement/AssigneeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpcenter.assigneemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssigneeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
